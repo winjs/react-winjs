ae72450e4e68a8b967926690d8aa5a3f8f4db63f
Amit Halberstam <amithalb@gmail.com>
2015-09-23 19:21:27 -0700

    Brand.ai documentation config

---
+++ docs/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+
+gulp.task("build", ["build:webpack", "build:winjs"]);
+
+/*
+ Copy over core winjs files to the dist folder
+ */
+gulp.task("build:winjs", function() {
+  return gulp.src([
+    'node_modules/winjs/js/base.min.js',
+    'node_modules/winjs/js/ui.min.js',
+    'node_modules/winjs/css/ui-light.min.css',
+    'node_modules/winjs/fonts/*'
+  ], { base: 'node_modules/winjs' }).pipe(gulp.dest('dist'))
+});
+
+/*
+ Build library
+ */
+gulp.task("build:webpack", function(callback) {
+  webpack(require('./webpack.config'), function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString());
+    callback();
+  });
+});
---
+++ docs/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+/*
+ Export react-winjs as a library object (a ReactWinJS var).
+ */
+module.exports = {
+  entry: './library.jsx',
+  output: {
+    filename: 'dist/browser-bundle.js',
+    library: 'ReactWinJS',
+    libraryTarget: 'var'
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx/, loader: 'jsx-loader'}
+    ]
+  },
+  externals: {
+    'react': 'React',
+    'react/addons': 'React'
+  }
+};
