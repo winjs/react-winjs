fc3b8e6e741a1ca52cb6935ff8d796e1c91d73b5
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-08 12:25:26 -0400

    ci(travis): fix invalid YAML

---
+++ ci/scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Deploy in develop
+if [[ $TRAVIS_BRANCH == 'develop' ]]; then
+    if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != "build(react-winjs): transpile and minify library" ]]; then    
+        # Generate CHANGELOG.md and increment version
+        yarn run release
+        # Get version number from package.json
+        export GIT_TAG=$(jq -r ".version" package.json)
+        # Revert last commit
+        git reset --hard HEAD~1
+        # Transpile and minify library
+        yarn run build
+        git add react-winjs.min.js
+        git commit -m "build(react-winjs): transpile and minify library for version $GIT_TAG"    
+        # push commits to develop
+        git push origin $TRAVIS_BRANCH
+    fi    
+fi
+
+# Deploy in master
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != "build(react-winjs): transpile and minify library" ]]; then    
+        # Generate CHANGELOG.md and increment version
+        yarn run release -- -m 'ci(release): generate CHANGELOG.md for version %s'
+        # Get version number from package.json
+        export GIT_TAG=$(jq -r ".version" package.json)
+        yarn run build
+        git add react-winjs.min.js
+        git commit -m "build(react-winjs): transpile and minify library for version $GIT_TAG"
+        # push commits to master
+        git push origin $TRAVIS_BRANCH
+    fi
+fi
