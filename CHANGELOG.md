ffe33344685d64408b7aafb17958776950a935f8
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-08 09:49:53 -0400

    ci(travis): add provider to master

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Change Log
-
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
-<a name="2.5.3"></a>
-## [2.5.3](https://github.com/winjs/react-winjs/compare/2.5.2...2.5.3) (2017-11-08)
--- ci/scripts/deploy_master.sh
+++ ci/scripts/deploy_master.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version
     git add .
     git commit -m "build(react-winjs): transpile and minify library"
     # Generate CHANGELOG.md and increment version
-    yarn run release -m 'ci(release): generate CHANGELOG.md for version %s'
+    yarn run release -- -m 'ci(release): generate CHANGELOG.md for version %s'
     # push commits to master
     git push origin $TRAVIS_BRANCH
 fi
