dadf3f7ab06cca7beabae22562d74c0c757c197d
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-11-08 10:39:45 -0400

    ci(deploy): add version in build commit

--- ci/scripts/deploy_master.sh
+++ ci/scripts/deploy_master.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $TRAVIS_COMMIT_MESSAGE != "build(react-winjs): transpile and minify library" ]]; then    
-    yarn run build
-    git add .
-    git commit -m "build(react-winjs): transpile and minify library"
     # Generate CHANGELOG.md and increment version
     yarn run release -- -m 'ci(release): generate CHANGELOG.md for version %s'
+    # Get version number from package.json
+    export GIT_TAG=$(jq -r ".version" package.json)
+    yarn run build
+    git add react-winjs.min.js
+    git commit -m "build(react-winjs): transpile and minify library for version $GIT_TAG"
     # push commits to master
     git push origin $TRAVIS_BRANCH
 fi
